5bcf39ce06dd968ebfa122ea581c56da34a611f2
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-04-01 02:39:59 +0530

    Time: 7 ms (86.53%) | Memory: 46.9 MB (91.13%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
@@ -3,7 +3,8 @@ class Solution {
       int row = strs.length;
       int col = strs[0].length();
       int count =0;
-      for(int i=0;i<col;i++){
+      //have to traverse letter by letter
+      for(int i=0;i<col;i++){// 0th letter
         for(int j=1;j<row;j++){
             if(strs[j].charAt(i)<strs[j-1].charAt(i)){ count++; break;}//move to next coulumn if lexicographically sorted
         }
